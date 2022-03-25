f3a14d3462053527208724d7303795bdf6677c5a
frankie <fviramontes8@unm.edu>
2022-03-25 15:26:28 -0600

    Update Vector class
    I am madge and want to cause a merg conflict to show the homies

--- VectorClass/dynamvector.hpp
+++ VectorClass/dynamvector.hpp
@@ -1,23 +1,23 @@
 #include <cstddef>
 #include <iostream>
 
-// ~std::array
+// This will be a class implementation of std::array with int
 class Vector {
 	private:
 		int *_values;
-		std::size_t _size;
+		size_t _size;
 
 	public:
 		Vector();
-		Vector(std::size_t size);
+		Vector(size_t size);
 		Vector(const Vector &v);
 
 		~Vector();
 
-		std::size_t size() const;
+		size_t size() const;
 		void print() const;
 
 		Vector &operator=(const Vector &v);
-		int &operator[](std::size_t idx);
+		int &operator[](size_t idx);
 		friend std::ostream &operator<<(std::ostream &out, const Vector &v);
 };
